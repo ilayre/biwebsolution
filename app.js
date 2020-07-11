2ed640eb7fb9bef601403f6367dd19cba5c746f8
ilayre <44551308+ilayre@users.noreply.github.com>
2020-07-11 22:32:36 +0300

    initial app

---
+++ app.js
@@ -0,0 +1,31 @@
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const dotenv = require("dotenv");
+
+
+const express = require("express");
+const app = express();
+
+dotenv.config();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+	res.render("./index");
+});
+
+app.get("/about", function(req, res){
+	res.render("about");
+});
+
+
+app.listen(3000, function(){
+	console.log("server listen on port 3000");
+});
+
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+// 	console.log("Server has started");
+// });
